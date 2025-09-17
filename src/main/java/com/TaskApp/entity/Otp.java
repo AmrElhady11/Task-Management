0be324889efb6dbb8be14f26a3cbcea75c24ae15
AmrElhady11 <amr.elhady.programs@gmail.com>
2025-09-17 19:43:47 +0300

    implement otp entity class

---
+++ src/main/java/com/TaskApp/entity/Otp.java
@@ -0,0 +1,33 @@
+package com.TaskApp.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "otp")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Otp {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "OTP", length = 20, nullable = false)
+    private String otp;
+
+    @Column(name = "expiration_time", nullable = false)
+    private LocalDateTime expirationTime;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+}
