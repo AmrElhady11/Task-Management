bb793a61a7304963185888725f9105c316264a78
AmrElhady11 <amr.elhady.programs@gmail.com>
2025-09-17 20:41:05 +0300

    implement openApi doc

--- src/main/java/com/TaskApp/config/AppConfig.java
+++ src/main/java/com/TaskApp/config/AppConfig.java
@@ -2,21 +2,52 @@ package com.TaskApp.config;
 
 
 import com.TaskApp.repository.UserRepository;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.servers.Server;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import java.util.Properties;
+
 @Configuration
 @RequiredArgsConstructor
-
+@OpenAPIDefinition(
+        info = @Info(
+                title = "OpenApi for Toda App",
+                version = "1.00",
+                description = "Open api for authentication-service as a part of Toda-App",
+                termsOfService = "Terms of service",
+                contact = @Contact(
+                        name = "amr",
+                        email = "amr.elhady@gmail.com"
+                ),
+                license = @License(
+                        name = "licence name",
+                        url = "https://url.com"
+                )
+        ),
+        servers = {
+                @Server(
+                        description = "local host",
+                        url = "http://localhost:8080"
+                )
+        }
+)
 
 public class AppConfig {
     public final UserRepository userRepository;
